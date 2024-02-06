6e559cc476d3601365c559ab725416c8b466555e
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-06 20:59:31 -0300

    feat: set Header style

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
 import logo from "./assets/logo-nlw-expert.svg";
 
 export function App() {
-    return <img src={logo} alt="logo" />;
+    return (
+        <div className="mx-auto max-w-6xl my-12 space-y-6">
+            <img src={logo} alt="logo-nlw-expert" />
+        </div>
+    );
 }
