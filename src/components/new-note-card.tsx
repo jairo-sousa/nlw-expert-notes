02810b868cf3215784217cd9c75992ea0a3c88f6
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-07 14:26:31 -0300

    feat: handle DialogClose click

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -9,9 +9,13 @@ export function NewNoteCard() {
         setShouldShowOnboarding(false);
     }
 
+    function handleEndEditor() {
+        setShouldShowOnboarding(true);
+    }
+
     function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
         if (event.target.value == "") {
-            setShouldShowOnboarding(true);
+            handleEndEditor();
         }
     }
 
@@ -30,7 +34,9 @@ export function NewNoteCard() {
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50" />
                 <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
-                    <Dialog.DialogClose className="absolute right-0 top-0 bg bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+                    <Dialog.DialogClose
+                        className="absolute right-0 top-0 bg bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100"
+                        onClick={handleEndEditor}>
                         <X className="size-5" />
                     </Dialog.DialogClose>
                     <div className="flex flex-1 flex-col gap-3 p-5">
