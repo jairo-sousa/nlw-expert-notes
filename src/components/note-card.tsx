17dd59b7aa9b62d345c61a181a92e04735381221
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-07 12:06:28 -0300

    feat: set Dialog structure

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from "@radix-ui/react-dialog";
+
 interface NoteCardProps {
     note: {
         date: Date;
@@ -7,13 +9,22 @@ interface NoteCardProps {
 
 export function NoteCard({ note }: NoteCardProps) {
     return (
-        <button className="rounded-md text-left flex flex-col gap-3 bg-slate-800 p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-            <span className="text-sm font-medium text-slate-300">
-                {note.date.toISOString()}
-            </span>
-            <p className="text-sm leading-6 text-slate-400">{note.content}</p>
+        <Dialog.Root>
+            <Dialog.Trigger className="rounded-md text-left flex flex-col gap-3 bg-slate-800 p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">
+                    {note.date.toISOString()}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    {note.content}
+                </p>
 
-            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
-        </button>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+                <Dialog.Overlay />
+                <Dialog.Content>Hi</Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     );
 }
