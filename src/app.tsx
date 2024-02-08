210790727d256acd81a1e25dd6d84543149545e7
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-07 22:48:19 -0300

    feat: use onNoteCreated to add notes

--- src/app.tsx
+++ src/app.tsx
@@ -12,6 +12,16 @@ export function App() {
         },
     ]);
 
+    function onNoteCreated(content: string) {
+        const newNote = {
+            id: Math.random(),
+            date: new Date(),
+            content,
+        };
+
+        setNotes([newNote, ...notes]);
+    }
+
     return (
         <div className="mx-auto max-w-6xl my-12 space-y-6">
             <img src={logo} alt="logo-nlw-expert" />
@@ -27,7 +37,7 @@ export function App() {
             <div className="h-px bg-slate-700" />
 
             <div className="grid grid-cols-3 gap-6 auto-rows-[250px] ">
-                <NewNoteCard />
+                <NewNoteCard onNoteCreated={onNoteCreated} />
                 {notes.map((note) => {
                     return <NoteCard key={note.id} note={note} />;
                 })}
