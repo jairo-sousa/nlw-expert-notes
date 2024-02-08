e27fc9a9e1f943bb624a26ed6344bd4f4bffcb1d
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-08 01:13:31 -0300

    feat: set onNoteDeleted

--- src/app.tsx
+++ src/app.tsx
@@ -30,7 +30,13 @@ export function App() {
         const notesArray = [newNote, ...notes];
 
         setNotes(notesArray);
+        localStorage.setItem("notes", JSON.stringify(notesArray));
+    }
 
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter((note) => note.id != id);
+
+        setNotes(notesArray);
         localStorage.setItem("notes", JSON.stringify(notesArray));
     }
 
@@ -66,7 +72,13 @@ export function App() {
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] ">
                 <NewNoteCard onNoteCreated={onNoteCreated} />
                 {filteredNotes.map((note) => {
-                    return <NoteCard key={note.id} note={note} />;
+                    return (
+                        <NoteCard
+                            key={note.id}
+                            note={note}
+                            onNoteDeleted={onNoteDeleted}
+                        />
+                    );
                 })}
             </div>
         </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,12 +5,14 @@ import { X } from "lucide-react";
 
 interface NoteCardProps {
     note: {
+        id: string;
         date: Date;
         content: string;
     };
+    onNoteDeleted: (id: string) => void;
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
     return (
         <Dialog.Root>
             <Dialog.Trigger className="rounded-md text-left flex flex-col gap-3 bg-slate-800 p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -49,9 +51,14 @@ export function NoteCard({ note }: NoteCardProps) {
                         type="button"
                         className="w-full bg bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font font-medium group">
                         Deseja{" "}
-                        <span className="text-red-400 group-hover:underline">
+                        <button
+                            onClick={() => {
+                                onNoteDeleted(note.id);
+                            }}
+                            type="button"
+                            className="text-red-400 group-hover:underline">
                             apagar essa nota
-                        </span>
+                        </button>
                         ?
                     </button>
                 </Dialog.Content>
