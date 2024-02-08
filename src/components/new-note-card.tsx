a49e33f791e29e71cd841643d3e50bc1e3cbf2a3
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-07 23:46:52 -0300

    feat: handle start recording visual effects

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -9,6 +9,7 @@ interface NewNoteCardProps {
 
 export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
+    const [isRecording, setIsRecording] = useState(false);
     const [content, setContent] = useState("");
 
     function handleStartEditor() {
@@ -37,6 +38,14 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
         setShouldShowOnboarding(true);
     }
 
+    function handleStartRecording() {
+        setIsRecording(true);
+        // setShouldShowOnboarding(false);
+    }
+    function handleStopRecording() {
+        setIsRecording(false);
+    }
+
     return (
         <Dialog.Root>
             <Dialog.Trigger className="rounded-md flex flex-col gap-3 text-left bg-slate-700 p-5 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -59,7 +68,7 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                     </Dialog.DialogClose>
 
                     <form
-                        onSubmit={handleSaveNote}
+                        // onSubmit={(e) => e.preventDefault()}
                         className="flex flex-col flex-1">
                         <div className="flex flex-1 flex-col gap-3 p-5">
                             <span className="text-sm font-medium text-slate-300">
@@ -68,11 +77,15 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                             {shouldShowOnboarding ? (
                                 <p className="text-sm leading-6 text-slate-400">
                                     Comece{" "}
-                                    <button className="font-md text-lime-400 hover:underline">
+                                    <button
+                                        type="button"
+                                        className="font-md text-lime-400 hover:underline"
+                                        onClick={handleStartRecording}>
                                         gravando uma nota
                                     </button>{" "}
                                     em áudio ou se preferir{" "}
                                     <button
+                                        type="button"
                                         className="font-md text-lime-400 hover:underline"
                                         onClick={handleStartEditor}>
                                         utilize apenas texto
@@ -89,12 +102,22 @@ export function NewNoteCard({ onNoteCreated }: NewNoteCardProps) {
                             )}
                         </div>
 
-                        <button
-                            type="submit"
-                            className="w-full bg bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font font-medium hover:bg-lime-500"
-                            onClick={handleSaveNote}>
-                            Salvar nota
-                        </button>
+                        {isRecording ? (
+                            <button
+                                type="button"
+                                className="w-full flex items-center justify-center gap-2 bg bg-slate-900 py-4 text-center text-sm text-slate-300 outline-none font font-medium hover:teext-slate-100"
+                                onClick={handleStopRecording}>
+                                <div className="size-3 rounded-full bg-red-500 animate-pulse" />
+                                Gravando! (clique p/ interromper)
+                            </button>
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSaveNote}
+                                className="w-full bg bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font font-medium hover:bg-lime-500">
+                                Salvar nota
+                            </button>
+                        )}
                     </form>
                 </Dialog.Content>
             </Dialog.Portal>
