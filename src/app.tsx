1a3d6d1712d2ba9b32b3c2aeef4cc40d360c59e1
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-08 00:55:54 -0300

    feat: make app responsive

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
-import { Search } from "lucide-react";
 
 interface Note {
     id: string;
@@ -50,7 +49,7 @@ export function App() {
             : notes;
 
     return (
-        <div className="mx-auto max-w-6xl my-12 space-y-6">
+        <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
             <img src={logo} alt="logo-nlw-expert" />
 
             <form className="w-full">
@@ -64,7 +63,7 @@ export function App() {
 
             <div className="h-px bg-slate-700" />
 
-            <div className="grid grid-cols-3 gap-6 auto-rows-[250px] ">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] ">
                 <NewNoteCard onNoteCreated={onNoteCreated} />
                 {filteredNotes.map((note) => {
                     return <NoteCard key={note.id} note={note} />;
